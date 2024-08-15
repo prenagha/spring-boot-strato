0959ececcbd44df39a229407987d69c488cd617d
Padraic Renaghan <padraic@renaghan.com>
2024-08-15 17:56:51 -0400

    working index page

--- app/src/main/java/com/renaghan/todo/IndexController.java
+++ app/src/main/java/com/renaghan/todo/IndexController.java
@@ -1,5 +1,6 @@
 package com.renaghan.todo;
 
+import java.security.Principal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,7 +11,7 @@ public class IndexController {
   public IndexController() {}
 
   @RequestMapping(method = RequestMethod.GET, path = "/")
-  public String getIndex() {
+  public String getIndex(Principal principal) {
     return "index";
   }
 }
