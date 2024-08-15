13e2f6c65e24c86168625c1297e9eff3e04e7565
Padraic Renaghan <padraic@renaghan.com>
2024-08-15 12:38:01 -0400

    no args needed

--- app/src/main/java/com/renaghan/todo/IndexController.java
+++ app/src/main/java/com/renaghan/todo/IndexController.java
@@ -1,7 +1,6 @@
 package com.renaghan.todo;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,7 +11,7 @@ public class IndexController {
 
   @GetMapping
   @RequestMapping("/")
-  public String getIndex(Model model) {
+  public String getIndex() {
     return "index";
   }
 }
